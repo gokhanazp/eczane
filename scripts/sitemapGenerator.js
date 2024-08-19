cc2e2b4379dddcda2ed9e4b5334d6f104c20921e
anilsrgt <anilsorgit@outlook.com.tr>
2024-08-19 16:04:27 +0300

    edit sitemap

--- scripts/sitemapGenerator.js
+++ scripts/sitemapGenerator.js
@@ -12,33 +12,41 @@ const sitemapGenerator = async () => {
       districtsByCity[city.slug] = districts.map(district => district.slug);
     }
 
+    const date = new Date().toISOString().split("T")[0];
     let sitemap = '<?xml version="1.0" encoding="utf-8" standalone="yes" ?>';
     sitemap += '\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
-    const date = new Date().toISOString().split("T")[0];
+    sitemap += "\n\t<url>";
+    sitemap += "\n\t\t<loc>https://www.turkiyenobetcieczane.com</loc>";
+    sitemap += `\n\t\t<lastmod>${date}</lastmod>`;
+    sitemap += "\n\t\t<changefreq>always</changefreq>";
+    sitemap += "\n\t\t<priority>1.0</priority>";
+    sitemap += "\n\t</url>";
+    sitemap += "\n\t<url>";
+    sitemap += "\n\t\t<loc>https://www.turkiyenobetcieczane.com/enyakinnobetcieczane</loc>";
+    sitemap += `\n\t\t<lastmod>${date}</lastmod>`;
+    sitemap += "\n\t\t<changefreq>always</changefreq>";
+    sitemap += "\n\t\t<priority>0.5</priority>";
+    sitemap += "\n\t</url>";
     for (const city in districtsByCity) {
       sitemap += "\n\t<url>";
       sitemap += `\n\t\t<loc>https://www.turkiyenobetcieczane.com/nobetcieczane/${city}</loc>`;
       sitemap += `\n\t\t<lastmod>${date}</lastmod>`;
       sitemap += "\n\t\t<changefreq>always</changefreq>";
-      sitemap += "\n\t\t<priority>N/A</priority>";
+      sitemap += "\n\t\t<priority>0.5</priority>";
       sitemap += "\n\t</url>";
       for (const district of districtsByCity[city]) {
         sitemap += "\n\t<url>";
         sitemap += `\n\t\t<loc>https://www.turkiyenobetcieczane.com/nobetcieczane/${city}/${district}</loc>`;
         sitemap += `\n\t\t<lastmod>${date}</lastmod>`;
         sitemap += "\n\t\t<changefreq>always</changefreq>";
-        sitemap += "\n\t\t<priority>N/A</priority>";
+        sitemap += "\n\t\t<priority>0.5</priority>";
         sitemap += "\n\t</url>";
       }
     }
     sitemap += "\n</urlset>";
 
-    const outputsDir = path.join(__dirname, "./outputs");
-    if (!fs.existsSync(outputsDir)) {
-      fs.mkdirSync(outputsDir);
-    }
-    const dir = path.join(outputsDir, "sitemap.xml");
-    fs.writeFileSync(dir, sitemap);
+    const sitemapPath = path.resolve(__dirname, "../public/sitemap.xml");
+    fs.writeFileSync(sitemapPath, sitemap);
     console.log("Sitemap generated successfully!");
   } catch (error) {
     console.error(error);
